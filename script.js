5e0d64a9e5f610bfc37cb28ada32010a30f66060
LUIS GIOLITO <luis.giolito@gmail.com>
2022-04-10 20:23:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function goLogin() {
+  window.location.href =
+    "file:///L:/Desktop/ArgentinaPrograma%232/Portfolio/login.html";
+}
+function goRegister() {
+  window.location.href =
+    "file:///L:/Desktop/ArgentinaPrograma%232/Portfolio/register.html";
+}
+
+function goHome() {
+  window.location.href =
+    "file:///L:/Desktop/ArgentinaPrograma%232/Portfolio/index2.html";
+}
